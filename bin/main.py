4911526db48e4e3d2b45a203b23e32c27a144951
Xuanlong <qxlpku@gmail.com>
2023-07-28 16:58:02 +0800

    add osgood factor dimensions pairs

--- bin/main.py
+++ bin/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 from lib import utils_fun,middleware,remain
 
 Yamlcon = utils_fun.YamlConfig()
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     competent_pairs_path = data_yaml["SCM"]["competent_pairs"]
     evaluation_pairs_path = data_yaml["SD"]["evaluation"]
     potency_pairs_path = data_yaml["SD"]["potency"]
+    activity_pairs_path = data_yaml["SD"]["activity"]
     
     # SCM dimensions
     ant_pairs_warmth = utils_fun.get_antonym_pairs(warmth_pairs_path)
@@ -18,24 +20,23 @@ if __name__ == "__main__":
     # SD dimensions
     ant_pairs_evaluation = utils_fun.get_antonym_pairs(evaluation_pairs_path)
     ant_pairs_potency = utils_fun.get_antonym_pairs(potency_pairs_path)
-    
+    ant_pairs_activity = utils_fun.get_antonym_pairs(activity_pairs_path)
     
     # filter pairs, condition cossim < 0.5
-    warm_pair = middleware.filter_pairs(ant_pairs_warmth)
-    competent_pair = middleware.filter_pairs(ant_pairs_competent)
-    evaluation_pair = middleware.filter_pairs(ant_pairs_evaluation)
-    potency_pair = middleware.filter_pairs(ant_pairs_potency)
+    # warm_pair = middleware.filter_pairs(ant_pairs_warmth)
+    # competent_pair = middleware.filter_pairs(ant_pairs_competent)
+    # evaluation_pair = middleware.filter_pairs(ant_pairs_evaluation)
+    # potency_pair = middleware.filter_pairs(ant_pairs_potency)
 
     
     ## build SCM dimensions , warmth and competent
-    dir_warmth = utils_fun.build_dimensions(warm_pair)
-    dir_competent = utils_fun.build_dimensions(competent_pair)
-    # print(dir_warmth.shape,dir_competent.shape)
- 
+    dir_warmth = utils_fun.build_dimensions(ant_pairs_warmth)
+    dir_competent = utils_fun.build_dimensions(ant_pairs_competent)
+
     # build SD dimensions, evaluation and potency
-    dir_evaluation = utils_fun.build_dimensions(evaluation_pair)
-    dir_potency = utils_fun.build_dimensions(potency_pair)
-    # print(dir_evaluation,dir_potency)
+    dir_evaluation = utils_fun.build_dimensions(ant_pairs_evaluation)
+    dir_potency = utils_fun.build_dimensions(ant_pairs_potency)
+    dir_activity = utils_fun.build_dimensions(ant_pairs_activity)
     
     # calcute cossim
     cos_sim_w_c = remain.cosine_similarity(dir_warmth,dir_competent)
@@ -43,8 +44,14 @@ if __name__ == "__main__":
     
     cos_sim_e_c = remain.cosine_similarity(dir_evaluation,dir_competent)
     cos_sim_e_w = remain.cosine_similarity(dir_evaluation,dir_warmth)
+    cos_sim_e_a = remain.cosine_similarity(dir_evaluation,dir_activity)
+
     cos_sim_p_c = remain.cosine_similarity(dir_potency,dir_competent)
     cos_sim_p_w = remain.cosine_similarity(dir_potency,dir_warmth)
+    cos_sim_p_a = remain.cosine_similarity(dir_potency,dir_activity)
+    
+    cos_sim_a_w = remain.cosine_similarity(dir_activity,dir_warmth)
+    cos_sim_a_c = remain.cosine_similarity(dir_activity,dir_competent)
  
     
     print("The cosine similarity between different dimensions.")
@@ -53,15 +60,15 @@ if __name__ == "__main__":
     print("**** Comparison ****")
     print("evaluation and competent: ", cos_sim_e_c, utils_fun.get_angel(cos_sim_e_c))
     print("evaluation and warmth: ", cos_sim_e_w, utils_fun.get_angel(cos_sim_e_w))
+    print("evaluation and activity: ", cos_sim_e_a, utils_fun.get_angel(cos_sim_e_a))
     
     print("potency and competent: ", cos_sim_p_c, utils_fun.get_angel(cos_sim_p_c))
     print("potency and warmth: ", cos_sim_p_w, utils_fun.get_angel(cos_sim_p_w))
+    print("potency and activity: ", cos_sim_p_a, utils_fun.get_angel(cos_sim_p_a))
+      
+    print("activity and warmth: ", cos_sim_a_w, utils_fun.get_angel(cos_sim_a_w))
+    print("activity and competent: ", cos_sim_a_c, utils_fun.get_angel(cos_sim_a_c))
     
-    # dims_scm = np.vstack((dir_warmth,dir_competent))
-    # orthogonal_dims = remain.orthogonal_dimensions(dims_scm)
-    # print(orthogonal_dims[0])
-    # project_w_c = middleware.new_semantic(orthogonal_dims)
-    # print(project_w_c)
-    # dims_sd = np.vstack((dir_evaluation,dir_potency))
+
     
     
