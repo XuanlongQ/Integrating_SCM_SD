47b95812e55dde4bffc6455428444c9b3c6e4e78
Xuanlong <qxlpku@gmail.com>
2023-08-01 20:42:19 +0800

    add osgood 50 pairs and its factor analysis

--- bin/func_factor.py
+++ bin/func_factor.py
@@ -41,7 +41,7 @@ def create_matrix(ant_pairs_Jenkins):
 
 if __name__ == "__main__":
     # Get the path
-    filep = data_yaml["SCM"]["competent_pairs"]
+    filep = data_yaml["Osgood"]["50_pairs"]
     ant_pairs_filep = utils_fun.get_antonym_pairs(filep)
     data = create_matrix(ant_pairs_filep)
     df = pd.DataFrame(data, index=["V" + str(i) for i in range(1, 301)])
